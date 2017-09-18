eda9705e03b71f684456af31fd581439918c84e4
nel215 <otomo.yuhei@gmail.com>
2017-09-18 15:22:58 +0900

    Add .gitignore

--- mymodule/database.py
+++ mymodule/database.py
@@ -4,7 +4,7 @@ from aiopg.sa import create_engine
 
 class EngineCreater(object):
     async def create(self):
-        return create_engine(
+        return await create_engine(
             user='user',
             password='pass',
             host='db',
